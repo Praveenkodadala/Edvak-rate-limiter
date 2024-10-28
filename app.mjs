83511004ec0dcc28b519fc9d8a855be4cf8fe14d
Praveenkodadala <55790663+Praveenkodadala@users.noreply.github.com>
2024-10-28 23:57:04 +0530

    rate limiter middleware is added and working fine

--- app.mjs
+++ app.mjs
@@ -1,12 +1,19 @@
-import express from 'express';
+import express from "express";
+import rateLimiter from "./middlewares/rateLimiter.middleware.mjs";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Server is working fine');
+app.get("/", rateLimiter, (req, res) => {
+	res.send("Server is working fine");
+});
+
+app.get("/unlimited", (req, res) => {
+	res.send("This route does not have rate limiting");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
